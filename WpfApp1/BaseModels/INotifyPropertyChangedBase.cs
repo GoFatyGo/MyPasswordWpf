a44480bcea7e98cd2d23132880a5447f0693730a
BudX_ol75@student.itstep.org <BudX_ol75@student.itstep.org>
2022-02-05 18:46:03 +0200

    Добавьте файлы проекта.

---
+++ WpfApp1/BaseModels/INotifyPropertyChangedBase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WpfApp01.BaseModels
+{
+    /// <summary>
+    /// Базовый класс для моделей к которым будет осущесвляться привязка
+    /// </summary>
+    public abstract class INotifyPropertyChangedBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // This method is called by the Set accessor of each property.
+        // The CallerMemberName attribute that is applied to the optional propertyName
+        // parameter causes the property name of the caller to be substituted as an argument.
+        protected void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
+
